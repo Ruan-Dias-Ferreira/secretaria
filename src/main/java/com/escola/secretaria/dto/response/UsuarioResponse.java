1ed16cd2eab7a9f5d1d592022f4f3b6af3d8f7f3
Ruan-Dias-p911-79 <ruandiasferrera@gmail.com>
2026-04-28 21:30:22 -0300

    modificação do usuario, para que o front possa se comunicar com id

--- src/main/java/com/escola/secretaria/dto/response/UsuarioResponse.java
+++ src/main/java/com/escola/secretaria/dto/response/UsuarioResponse.java
@@ -3,6 +3,7 @@ package com.escola.secretaria.dto.response;
 import com.escola.secretaria.domain.enums.Role;
 
 public record UsuarioResponse(
+        Long id,
         String login,
         Role role
 ) {
